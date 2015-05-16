b66d4261931e7c97d569180d6b8cf273c5245865
Matías O'Keefe <mok@singular.local>
2015-05-15 22:47:03 -0300

    Mayusculizando

--- application/controllers/pros.php
+++ application/controllers/pros.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class pros extends CI_Controller {
+class Pros extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
